567489eda329cc6eac3e603bc8ac6f3bbb79ab8e
The itHelp Project <96568278+npovn@users.noreply.github.com>
2022-06-01 15:37:40 +0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,15 @@
+const btncontainer = document.querySelector(".burger_container");
+const btnmenu = document.querySelector(".burger_menu");
+let menuOpen = false;
+btncontainer.addEventListener("click", function(){
+    if(!menuOpen){
+        btncontainer.classList.add("open");
+        btnmenu.classList.add("open");
+        menuOpen = true;
+    }
+    else{
+        btncontainer.classList.remove("open");
+        btnmenu.classList.remove("open");
+        menuOpen = false;
+    }
+})
